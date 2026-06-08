e52ac80234cced6a72abe66b219035a4bb1839e7
Amit Mishra <mishraamitkumar644@gmail.com>
2026-06-08 12:29:19 +0530

    fix: terraform fmt formatting

--- environments/prod.tfvars
+++ environments/prod.tfvars
@@ -6,6 +6,6 @@ location                 = "East US"
 resource_group_name      = "rg-myproject-prod"
 storage_account_name     = "stmyprojectprod001"
 storage_account_tier     = "Standard"
-storage_replication_type = "GRS"        # Geo-redundant for prod
+storage_replication_type = "GRS" # Geo-redundant for prod
 project                  = "myproject"
 owner                    = "platform-team"
--- modules/storage_account/main.tf
+++ modules/storage_account/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_storage_account" "this" {
   https_traffic_only_enabled      = true
   min_tls_version                 = "TLS1_2"
   allow_nested_items_to_be_public = false
-  shared_access_key_enabled       = false   # Force Azure AD auth only
+  shared_access_key_enabled       = false # Force Azure AD auth only
   blob_properties {
     delete_retention_policy {
       days = var.blob_delete_retention_days
